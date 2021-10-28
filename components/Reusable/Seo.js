56117f0496b9c4d8e601cdd875932ef68c5cc897
Iykekelvins <kelvinochubili@gmail.com>
2021-10-28 13:54:09 +0100

    seo component created

--- components/Reusable/Seo.js
+++ components/Reusable/Seo.js
@@ -6,6 +6,7 @@ const Seo = ({ title }) => {
       <title>
         {`Frontend Mentor | Space tourism website ${title && title}`}
       </title>
+      <link rel="icon" href="/favicon-32x32.png" />
     </Head>
   );
 };
