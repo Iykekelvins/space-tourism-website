09e094b3ed0594f679d1466aa8cb613ae87ab758
Iykekelvins <kelvinochubili@gmail.com>
2021-10-28 13:43:59 +0100

    crew page

---
+++ pages/crew.js
@@ -0,0 +1,14 @@
+import { Layout, Seo, Crew } from "../components";
+
+const crew = () => {
+  return (
+    <div className="crew">
+      <Layout>
+        <Seo title="| Crew" />
+        <Crew />
+      </Layout>
+    </div>
+  );
+};
+
+export default crew;
