9bfd30dd32816df86ee71f9e8452a587fe56163b
Iykekelvins <kelvinochubili@gmail.com>
2021-10-28 13:44:28 +0100

    technology page

---
+++ pages/technology.js
@@ -0,0 +1,14 @@
+import { Technology, Layout, Seo } from "../components";
+
+const technology = () => {
+  return (
+    <section className="technology">
+      <Layout>
+        <Seo title="| Technology" />
+        <Technology />
+      </Layout>
+    </section>
+  );
+};
+
+export default technology;
