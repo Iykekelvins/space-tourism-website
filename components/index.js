b799e4fb936f7aa76de8b6f6618e3c69bacf8df6
Iykekelvins <kelvinochubili@gmail.com>
2021-10-28 13:43:31 +0100

    components created

---
+++ components/index.js
@@ -0,0 +1,30 @@
+//COMPONENTS
+import Layout from "./Reusable/Layout";
+import Navbar from "./Reusable/Navbar";
+import Logo from "./Reusable/Logo";
+import Seo from "./Reusable/Seo";
+import Button from "./Reusable/Button";
+
+//LANDING PAGE
+import HomeInfo from "./LandingPage/HomeInfo";
+
+//DESTINATION PAGE
+import Destination from "./DestinationPage/Destination";
+
+//TECHNOLOGY PAGE
+import Technology from "./TechnologyPage/Technology";
+
+//CREW PAGE
+import Crew from "./CrewPage/Crew";
+
+export {
+  Layout,
+  Navbar,
+  Logo,
+  Seo,
+  Button,
+  HomeInfo,
+  Destination,
+  Technology,
+  Crew,
+};
